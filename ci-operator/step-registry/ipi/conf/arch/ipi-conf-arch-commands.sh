8e2ffcaa56962bcf3e178a4d062eee50486f2ba5
deepsm007 <dmistry@redhat.com>
2020-11-10 12:43:37 -0500

    WIP fixing parameter errors in workflow and chain

--- ci-operator/step-registry/ipi/conf/arch/ipi-conf-arch-commands.sh
+++ ci-operator/step-registry/ipi/conf/arch/ipi-conf-arch-commands.sh
@@ -6,8 +6,38 @@ set -o pipefail
 
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
-#expiration_date=$(date -d '4 hours' --iso=minutes --utc)
+declare -A SUBNETS
+        SUBNETS["${CLUSTER_TYPE}-0-0"]="126"
+        SUBNETS["${CLUSTER_TYPE}-0-1"]="1"
+        SUBNETS["${CLUSTER_TYPE}-0-2"]="2"
+        SUBNETS["${CLUSTER_TYPE}-0-3"]="3"
+        SUBNETS["${CLUSTER_TYPE}-0-4"]="4"
+        SUBNETS["${CLUSTER_TYPE}-1-0"]="126"
+        SUBNETS["${CLUSTER_TYPE}-1-1"]="1"
+        SUBNETS["${CLUSTER_TYPE}-1-2"]="2"
+        SUBNETS["${CLUSTER_TYPE}-1-3"]="3"
+        SUBNETS["${CLUSTER_TYPE}-1-4"]="4"
 
+        # ensure LEASED_RESOURCE is set
+        if [[ -z "${LEASED_RESOURCE}" ]]; then
+          echo "Failed to acquire lease"
+          exit 1
+        fi
+
+        # get cluster subnet or default it to 126
+        cluster_subnet="${SUBNETS[${LEASED_RESOURCE}]}"
+        if [[ -z "$cluster_subnet" ]]; then
+           cluster_subnet=126
+        fi
+
+        # get cluster libvirt uri or default it the first host
+        remote_libvirt_uri="qemu+tcp://${LIBVIRT_HOSTS[${LEASED_RESOURCE}]}/system"
+        if [[ -z "$remote_libvirt_uri" ]]; then
+           remote_libvirt_uri="qemu+tcp://${REMOTE_LIBVIRT_HOSTNAME}/system"
+        fi
+        echo $remote_libvirt_uri
+        
+        NETWORK_NAME="br$(printf ${LEASED_RESOURCE} | tail -c 3)"
 
 cat >> "${CONFIG}" << EOF
 baseDomain: ${LEASED_RESOURCE}
@@ -34,6 +64,7 @@ compute:
 platform:
   libvirt:
     URI: ${remote_libvirt_uri}
-    network: ${NETWORK_NAME}
+    network: 
+      if: ${NETWORK_NAME}
 EOF
 
